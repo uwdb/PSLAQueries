7980efad6b3c3aef4fc4ca832a62b30d7fa57bbc
jortiz16 <jortiz16@uw.edu>
2015-07-09 09:29:18 -0700

    automate

--- queries/run_queries_multi-tenant.py
+++ queries/run_queries_multi-tenant.py
@@ -45,7 +45,7 @@ qPath = [
 		#"tpch/tpch-type3/tpch-type3b/12_datanodes/8_computenodes/",
 		#"tpch/tpch-type3/tpch-type3b/12_datanodes/10_computenodes/"
 
-		"tpch/multi-tenant/2_tenants-2_shared/",
+		#"tpch/multi-tenant/2_tenants-2_shared/",
 		#"tpch/multi-tenant/2_tenants-4_shared/",
 		#"tpch/multi-tenant/2_tenants-6_shared/",
 		#"tpch/multi-tenant/2_tenants-8_shared/",
@@ -75,25 +75,25 @@ qPath = [
 		#"tpch/multi-tenant/6_tenants-6_shared/",
 		#"tpch/multi-tenant/6_tenants-8_shared/"
 
-		#"tpch/multi-tenant/7_tenants-2_shared/",
-		#"tpch/multi-tenant/7_tenants-4_shared/",
+		"tpch/multi-tenant/case_663/7_tenants-2_shared/",
+		"tpch/multi-tenant/case_663/7_tenants-4_shared/",
 		#"tpch/multi-tenant/7_tenants-6_shared/",
 		#"tpch/multi-tenant/7_tenants-8_shared/"
 
-		#"tpch/multi-tenant/8_tenants-2_shared/",
-		#"tpch/multi-tenant/8_tenants-4_shared/",
+		"tpch/multi-tenant/case_663/8_tenants-2_shared/",
+		"tpch/multi-tenant/case_663/8_tenants-4_shared/",
 		#"tpch/multi-tenant/8_tenants-6_shared/",
 		#"tpch/multi-tenant/8_tenants-8_shared/"
 
-		#"tpch/multi-tenant/9_tenants-2_shared/",
-		#"tpch/multi-tenant/9_tenants-4_shared/",
+		"tpch/multi-tenant/case_663/9_tenants-2_shared/",
+		"tpch/multi-tenant/case_663/9_tenants-4_shared/",
 		#"tpch/multi-tenant/9_tenants-6_shared/",
 		#"tpch/multi-tenant/9_tenants-8_shared/"
 
 		#won't be able to run this
 		#"tpch/multi-tenant/10_tenants-2_shared/",
 		
-		#"tpch/multi-tenant/10_tenants-4_shared/",
+		"tpch/multi-tenant/case_663/10_tenants-4_shared/",
 		#"tpch/multi-tenant/10_tenants-6_shared/",
 		#"tpch/multi-tenant/10_tenants-8_shared/"
 		]
@@ -107,9 +107,13 @@ for p in qPath:
 	
 	qList = os.listdir("/root/PSLAQueries/queries/"+ p)
 
+	f = open(os.path.expanduser(p + "runtimes.txt"), 'w');
+
 	#call bash scripts
 	subprocess.call(['/bin/bash',"clear-mt.sh"]);
 	print("postgres and os cleared")
+
+	connection_mapper = []
 
 	#for each query in the path
 	for q in qList:
@@ -124,6 +128,7 @@ for p in qPath:
 			#try running the query
 			try:
 				query_status = connection.submit_query(data)
+				connection_mapper.append(query_status)
 				query_id = query_status['queryId']
 			except myria.MyriaError as e:
 				print("MyriaError")
@@ -132,3 +137,23 @@ for p in qPath:
 		except:
 			print "Query does not exist"
 			break
+
+	#wait for all to finish
+	done = False
+	while not done:
+		done = True
+		for c in connection_mapper:
+			status = int((c.get_query_status(query_id))['elapsedNanos'])
+			if status!='SUCCESS':
+				done = False
+
+	#record the runtimes
+	f.write(p)
+	for c in connection_mapper:
+		totalElapsedTime = int((connection.get_query_status(query_id))['elapsedNanos'])
+		f.write(totalElapsedTime)
+
+	f.flush()
+	f.close()
+
+
