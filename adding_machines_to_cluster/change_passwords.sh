35f8b4cb0f414e236e8dc4710419c1b64563cc47
jortiz16 <jortiz16@uw.edu>
2015-08-12 20:53:17 -0700

    removing files

--- adding_machines_to_cluster/change_passwords.sh
+++
@@ -1,2 +0,0 @@
-ssh machine-example-1@amazon.com 'ALTER USER uwdb WITH PASSWORD 'p47f8iqnct'' | sudo -u postgres psql myria
-ssh machine-example-2@amazon.com 'ALTER USER uwdb WITH PASSWORD 'p47f8iqnct'' | sudo -u postgres psql myria
--- adding_machines_to_cluster/passwordless_ssh.sh
+++
@@ -1,2 +0,0 @@
-cat ~/.ssh/id_rsa.pub | ssh -i jortiz16Key.rsa root@machine-example-1@amazon.com'cat >> .ssh/authorized_keys'
-cat ~/.ssh/id_rsa.pub | ssh -i jortiz16Key.rsa root@machine-example-2@amazon.com'cat >> .ssh/authorized_keys'
