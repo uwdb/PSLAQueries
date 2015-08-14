1ab9d61eed68f1d041427112956708df1bad605e
jortiz16 <jortiz16@uw.edu>
2015-08-14 12:27:25 -0700

    missing worker in script

--- queries/clear/clearOSCache-tpch.sh
+++ queries/clear/clearOSCache-tpch.sh
@@ -1,4 +1,4 @@
-host="mycluster-node001 mycluster-node002 mycluster-node003 mycluster-node004 mycluster-node005 mycluster-node006 mycluster-node007 mycluster-node008 mycluster-node009 mycluster-node010 mycluster-node012"
+host="mycluster-node001 mycluster-node002 mycluster-node003 mycluster-node004 mycluster-node005 mycluster-node006 mycluster-node007 mycluster-node008 mycluster-node009 mycluster-node010 mycluster-node011  mycluster-node012"
 
 CMD="free && sync && echo \"echo 1 > /proc/sys/vm/drop_caches\" | sudo sh"
 parallel -k --jobs +28 "/bin/echo -n '{} -- ' && ssh {} '$CMD'" ::: $host
--- queries/clear/postgresRestart-tpch.sh
+++ queries/clear/postgresRestart-tpch.sh
@@ -1,4 +1,4 @@
-host="mycluster-node001 mycluster-node002 mycluster-node003 mycluster-node004 mycluster-node005 mycluster-node006 mycluster-node007 mycluster-node008 mycluster-node009 mycluster-node010 mycluster-node012"
+host="mycluster-node001 mycluster-node002 mycluster-node003 mycluster-node004 mycluster-node005 mycluster-node006 mycluster-node007 mycluster-node008 mycluster-node009 mycluster-node010 mycluster-node011  mycluster-node012"
 
 CMD="sudo service postgresql restart"
 parallel -k --jobs +28 "/bin/echo -n '{} -- ' && ssh {} '$CMD'" ::: $host
